9a38bd91efd62c8a0b594e39efcc39b104e0d02f
xykt-fix <gansukh6got@gmail.com>
2026-05-15 19:55:05 +0800

    Add Shaka Player DRM configuration example

---
+++ dam.js
@@ -0,0 +1,8 @@
+// Example using Shaka Player configuration
+player.configure({
+  drm: {
+    servers: {
+      'com.widevine.alpha': 'https://voo.mn/drm.php/widevine?token=4c5ea894d219141aab618147b8c30998cf5f632'
+    }
+  }
+});
